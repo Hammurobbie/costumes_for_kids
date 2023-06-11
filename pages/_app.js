48401be3cf3c56dac6ed5a14e5b96133b4c14d91
hammurobbie <rthomporter@gmail.com>
2023-06-11 18:11:50 -0500

    vercel test

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,10 @@
+// TODO: add extra page of form gathering more info changing user experience based on age/favorites/costume or outfit etc
+// TODO: connect forms at least to next.js backend for demo
+// TODO: add more customization for sketchpad
+// TODO: add image upload
+// TODO: image gallery of user uploads and creator progress uploads
+// TODO: maybe chat functionality
+
 import "../styles/globals.css";
 import { useState } from "react";
 import { UserContext } from "../contexts/UserContext";
@@ -19,13 +26,6 @@ export default function App({ Component, pageProps }) {
     favColor: favColor,
     shortDesc: shortDesc,
   });
-
-  // TODO: add extra page of form gathering more info changing user experience based on age/favorites/costume or outfit etc
-  // TODO: connect forms at least to next.js backend for demo
-  // TODO: add more customization for sketchpad
-  // TODO: add image upload
-  // TODO: image gallery of user uploads and creator progress uploads
-  // TODO: maybe chat functionality
 
   return (
     <UserContext.Provider value={{ globalUser, setGlobalUser }}>
