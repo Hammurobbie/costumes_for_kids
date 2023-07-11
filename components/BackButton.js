9a31d457109b2d620eb811fda4cb3943e129cd48
hammurobbie <rthomporter@gmail.com>
2023-07-10 19:06:42 -0500

    back sub

--- components/BackButton.js
+++ components/BackButton.js
@@ -5,11 +5,12 @@ import { useRouter } from "next/router";
 import back_arrow from "../img/arrow-drawing.png";
 
 const BackButton = () => {
-  const { back } = useRouter();
+  const { push } = useRouter();
+  // const { back } = useRouter();
   return (
     <button className="absolute left-4 top-[21px] w-auto py-0.5 px-1 shadow-sm rounded-lg hover:bg-tangerine-alt focus:bg-tangerine-alt">
       <Image
-        onClick={() => back()}
+        onClick={() => push("/dashboard")}
         src={back_arrow}
         height="auto"
         width={25}
